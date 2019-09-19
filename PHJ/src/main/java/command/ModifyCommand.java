1acae8af215687552fc8122f6c14bbd951f00ff9
GwG <GwG@GwG-Notebook>
2019-09-19 12:13:44 +0900

    의 커밋

--- PHJ/src/main/java/command/ModifyCommand.java
+++ PHJ/src/main/java/command/ModifyCommand.java
@@ -1,11 +1,27 @@
 package command;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.ui.Model;
+
+import member.MemberDTO;
 
 public class ModifyCommand implements PHJCommandImpl{
 	
 	@Override
 	public void execute(Model model) {
+		
+		Map<String, Object> map = model.asMap();
+		HttpServletRequest req = (HttpServletRequest)map.get("req");
+		
+		//커맨드객체 가져오기.
+		MemberDTO dto = (MemberDTO)map.get("memberDTO");
+		
+		System.out.println("수정될 회원정보 로그 : " + dto.getPass());
+		
+		
 		
 		
 	}
